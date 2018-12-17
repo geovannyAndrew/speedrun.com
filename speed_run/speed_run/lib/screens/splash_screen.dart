b3879d8dcf4b3f6ff777d335c38d9f75f6ae5399
Geovanny Buitrago <geovanny91_1@hotmail.com>
2018-12-17 01:12:07 -0500

    Made a simple splash screen

---
+++ speed_run/speed_run/lib/screens/splash_screen.dart
@@ -0,0 +1,55 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:speed_run/screens/my_home_page.dart';
+
+class SplashScreen extends StatefulWidget{
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SplashScreenState();
+  }
+
+
+}
+
+
+class _SplashScreenState extends State<SplashScreen>{
+
+  _delayTimer() async{
+    var _duration = Duration(seconds: 3);
+    return Timer(_duration,_goToHomeScreen);
+  }
+
+  _goToHomeScreen(){
+    Navigator.pushReplacementNamed(context, "/home");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _delayTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Image.asset(
+            "assets/images/logo_speed_run.png",
+            width: 200.0,
+            fit: BoxFit.fitWidth,
+          )
+        ),
+      ),
+    );
+  }
+
+}
